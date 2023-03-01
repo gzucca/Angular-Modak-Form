a44c70d590ac12826f5cfc41f3f1786adf7242b9
gzucca <gonzalozucca_4@outlook.com>
2023-02-28 23:17:56 -0300

    Testing SASS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 
+
 @NgModule({
   declarations: [
     AppComponent,
